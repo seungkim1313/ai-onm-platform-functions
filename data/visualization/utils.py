b096b598b0f4e70bf278c2b43eb778db99fbef9f
Kim SeungKeun <seungkim1313@naver.com>
2023-03-06 16:20:59 +0900

    load images and its label for classification task is added

--- data/visualization/utils.py
+++ data/visualization/utils.py
@@ -1,20 +1,53 @@
+import numpy as np
 from pathlib import Path
 import PIL.Image as pilimg
 
-TaskNotFound = Exception(
+TaskNotFoundError = Exception(
     "Task that you provided is not valid."
 )
 
+InvalidParamError = Exception(
+    "Parameters that you provided is invalid."
+)
+
 def dataloader_image(task, list_img_path, list_label_path):
+    
+    list_img = []
+    list_label = []
+    
     if task == "classification":
-        pass
+        if list_img_path == None or len(list_img_path) < 1: 
+            raise InvalidParamError
+        for img_path in list_img_path:
+            if not img_path.exists():
+                raise FileNotFoundError
+            label = img_path.parent.name
+            img = np.array(pilimg.open(img_path))
+            list_img.append(img)
+            list_label.append(label)
+        return list_img, list_label
     elif task == "detection":
         pass
     elif task == "segmentation":
         pass
     else:
-        raise TaskNotFound
+        raise TaskNotFoundError
     
 
 if __name__ == "__main__":
-    pass
+    
+    ## dataloader_image input test
+    task = "classification"
+    list_img_path = [
+        Path("./sample_data/classification/paper/0a3UtNzl5Ll3sq8K.png"),
+        Path("./sample_data/classification/paper/0cb6cVL8pkfi4wF6.png")
+    ]
+    list_label_path = []
+    
+    a, b = dataloader_image(task, list_img_path, list_label_path)
+    print(len(a))
+    print(b)
+
+    
+    
+    
